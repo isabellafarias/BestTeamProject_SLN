43ed15eef6afc4ef932eff60993bda48ff0a3d7b
ifarias <ifarias@asu.edu>
2018-04-23 23:24:05 -0700

    styling

--- BestTeamProject/WebTemplate.Master.designer.cs
+++ BestTeamProject/WebTemplate.Master.designer.cs
@@ -49,6 +49,15 @@ namespace BestTeamProject {
         protected global::System.Web.UI.WebControls.TextBox searchTextBox;
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
