253673621de2acc5e4a2d259f8d34acecc0fd9df
ifarias <ifarias@asu.edu>
2018-04-23 22:37:37 -0700

    search results functionality

--- BestTeamProject/searchResultsPage.aspx.designer.cs
+++ BestTeamProject/searchResultsPage.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace BestTeamProject {
     public partial class searchResultsPage {
         
         /// <summary>
-        /// testingLbl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label testingLbl;
-        
-        /// <summary>
         /// resultsTable control.
         /// </summary>
         /// <remarks>
